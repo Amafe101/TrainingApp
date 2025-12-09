e52a5b3b732b16ccb04fdbc6227acf4b5d7ae6ce
Amafe101 <103948585+Amafe101@users.noreply.github.com>
2025-12-09 01:00:07 +0000

    new-updates

---
+++ assets/qrcode.js
@@ -0,0 +1,16 @@
+let imageUrl = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+let inputEl = document.getElementById("input-el");
+let imgEl = document.getElementById("qrImage");
+let mainUrl = inputEl.value;
+
+
+
+function generateCode(){
+    if (inputEl.value.length > 0 ) {
+    document.getElementById("qrImage").src = `${imageUrl} ${inputEl.value}`;
+    } else {
+        alert("Please enter some text...");
+    }
+
+}
+
